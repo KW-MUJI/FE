4eb75da5ce63219b5d2e4b42afc1e38f8777b9eb
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-09-03 16:30:17 +0900

    login&sing link#2
    css 수정

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,27 +2,21 @@ import React, { useState, useEffect } from 'react';
 import logo from '../assets/logo.png';
 import '../styles/Banner.css';
 
-export const Banner = () => {
-    return (
-  
-      <div className="banner-container">
-  
-        <img src={logo} className="logo" alt="logo" />
-        <div className='title'>
-          <h2>광운대학교 생활 도우미</h2>
-          <h1>광운 무인양품</h1>
-        </div>
-        <nav className="navbar">
-          <ul className="navbar-menu">
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#login">LOGIN</a></li>
-            <li><a href="#kw" className="k-menu">광운대학교</a></li>
-          </ul>
-        </nav>
-  
-      </div>
-  
-  
-    );
-  }
-  export default Banner;
+const Banner = () => (
+  <div className="banner-container">
+    <img src={logo} className="logo" alt="logo" />
+    <div className='title'>
+      <h2>광운대학교 생활 도우미</h2>
+      <h1>광운 무인양품</h1>
+    </div>
+    <nav className="navbar">
+      <ul className="navbar-menu">
+        <li><a href="#home">HOME</a></li>
+        <li><a href="#login">LOGIN</a></li>
+        <li><a href="#kw" className="k-menu">광운대학교</a></li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Banner;
