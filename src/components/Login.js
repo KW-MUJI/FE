553a19394d61a55adee77b92acc71e4cd1a87c51
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-09-14 12:44:52 +0900

    login_page#8
    login 확인 로직 추가

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef,useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from '../styles/Login.module.css'; // CSS 파일 임포트
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -6,9 +6,17 @@ import { Link, useNavigate } from 'react-router-dom';
 const Login = () => {
   const [email, setEmail] = useState(''); // 이메일 상태 선언 및 초기화
   const [password, setPassword] = useState(''); // 비밀번호 상태 선언 및 초기화
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태 선언
+  //const [errorMessage, setErrorMessage] = useState(''); // 에러 메시지 상태
+
+  const mockUser = {
+    email: 'test@kw.ac.kr',
+    password: '123456'
+  };
+
+
   const navigate = useNavigate();
   const inputRef = useRef(null); // 입력 필드 참조
-
   const fixedDomain = '@kw.ac.kr'; // 고정 도메인
 
 
@@ -20,31 +28,52 @@ const Login = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Logging in with', { email, password });
+    console.log('id : '+ email);
+    console.log('pw : '+ password);
+
+    const textElement = document.querySelector(`.${styles.checkLogin}`);
+
+    if (email === mockUser.email && password === mockUser.password) {
+      setIsLoggedIn(true); // 로그인 상태로 변경
+      navigate('/home'); 
+    }
+
+    else { 
+      alert('잘못된 아이디/비밀번호 입니다.');
+      setIsLoggedIn(false); // 로그인 상태로 변경
+
+      setEmail(''+fixedDomain);
+    
+      setPassword('');
+
+      }
   };
 
   const handleEmailChange = (e) => {
     const value = e.target.value;
 
-    
-      // 이메일 앞부분만 수정하고 '@kw.ac.kr'을 고정
-      if (value.endsWith(fixedDomain)) {
-        setEmail(value);
-      } 
-    
+
+    // 이메일 앞부분만 수정하고 '@kw.ac.kr'을 고정
+    if (value.endsWith(fixedDomain)) {
+      setEmail(value);
+    }
+
   };
 
   const handleFocus = () => {
     // 커서를 항상 고정된 도메인 앞에 위치시키기 위한 설정
     const inputElement = inputRef.current;
     const end = email.length - fixedDomain.length;
-    inputElement.setSelectionRange(end, end);  };
+    inputElement.setSelectionRange(end, end);
+  };
 
 
 
   const navigateToPrivacy = () => {
     window.open("/privacy_law", "_blank");
   };
+
+
 
   return (
     <div className={styles.login_container}>
@@ -71,7 +100,9 @@ const Login = () => {
             required
           />
         </div>
+        {/* <p className={styles.checkLogin}>　</p> */}
         <button className={styles.login_btn} type="submit">로그인</button>
+    
       </form>
       <div className={styles.login_options}>
         <Link to="/pwFind" className={styles.find_password_link}>비밀번호 찾기</Link>
