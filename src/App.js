f444a204b35daef649c0f7f1e9c38863a3f5423d
Eunhyang Cho <eunhyangcho@syn-172-100-005-200.res.spectrum.com>
2024-11-05 17:18:47 +0900

    rename: My팀플 폴더 생성 및 관련 파일 이동

--- src/App.js
+++ src/App.js
@@ -1,88 +1,77 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './assets/logo.png';
-import './App.css';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Banner from './components/Banner';
-import Footer from './components/Footer';
-import PwFind from './components/PwFind';
-import PwReset from './components/PwResrt';
-import Privacy_law from './components/Privacy_law';
-import RecruitWrite from './components/Recruit_write';
-import RecruitPost from './components/Recruit_post';
-import RecruitMain from './components/Recruit_main';
-import Survey from './components/Survey';
-import SurveyWrite from './components/Survey_write';
-import SurveyJoin from './components/Survey_join';
-import MyTeam from './components/My_team';
-import MyRecruitTeam from './components/My_recruit_team';
-import MyTeamApplicant from './components/My_team_applicant';
-import SurveyComplete from './components/Survey_complete';
-import MySurvey from './components/My_survey';
-import SurveyResult from './components/survet_result';
-import Schedule from './components/Schedule';
-import MyPage from './components/My_page';
-import CheckPw from './components/CheckPw';
-import Update from './components/Update';
-import MainPage from './components/mainpage';
-import Notice from './components/Notice';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import logo from "./assets/logo.png";
+import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import PwFind from "./components/PwFind";
+import PwReset from "./components/PwResrt";
+import Privacy_law from "./components/Privacy_law";
+import RecruitWrite from "./components/Recruit_write";
+import RecruitPost from "./components/Recruit_post";
+import RecruitMain from "./components/Recruit_main";
+import Survey from "./components/Survey";
+import SurveyWrite from "./components/Survey_write";
+import SurveyJoin from "./components/Survey_join";
+import MyTeam from "./components/MyTeam/My_team";
+import MyRecruitTeam from "./components/MyTeam/My_recruit_team";
+import MyTeamApplicant from "./components/MyTeam/My_team_applicant";
+import SurveyComplete from "./components/Survey_complete";
+import MySurvey from "./components/My_survey";
+import SurveyResult from "./components/survet_result";
+import Schedule from "./components/Schedule";
+import MyPage from "./components/My_page";
+import CheckPw from "./components/CheckPw";
+import Update from "./components/Update";
+import MainPage from "./components/mainpage";
+import Notice from "./components/Notice";
 
 // import Login from './components/Login';
-
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태 관리
 
-
   //로그아웃처리
   const handleLogout = () => {
     setIsLoggedIn(false); // 로그아웃 시 로그인 상태를 false로 변경
-
   };
-
-
 
   return (
     <Router>
       <div className="App">
         <Banner isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
         <Routes>
-
-          <Route path="/" element={<MainPage />} /> {/* 기본 경로를 Login으로 설정 */}
-         
-
+          <Route path="/" element={<MainPage />} />{" "}
+          {/* 기본 경로를 Login으로 설정 */}
           <Route path="/home" element={<MainPage />} />
-          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
+          <Route
+            path="/login"
+            element={<Login setIsLoggedIn={setIsLoggedIn} />}
+          />
           <Route path="/signup" element={<Signup />} />
           <Route path="/pwFind" element={<PwFind />} />
           <Route path="/pwReset" element={<PwReset />} />
           <Route path="/privacy_law" element={<Privacy_law />} />
-          
           <Route path="/recruit_write" element={<RecruitWrite />} />
           <Route path="/recruit_main" element={<RecruitMain />} />
           <Route path="/recruit_post/:id" element={<RecruitPost />} />
-
           <Route path="/survey" element={<Survey />} />
           <Route path="/survey_write" element={<SurveyWrite />} />
           <Route path="/My_survey" element={<MySurvey />} />
           <Route path="/survey_result" element={<SurveyResult />} />
-          <Route path="/survey_join" element={<SurveyJoin/>}/>
+          <Route path="/survey_join" element={<SurveyJoin />} />
           <Route path="/survey_complete" element={<SurveyComplete />} />
-          
-          <Route path="/my_team" element={<MyTeam/>}/>
-          <Route path="/my_recruit_team" element={<MyRecruitTeam/>}/>
-          <Route path="/my_team_applicant/:id" element={<MyTeamApplicant/>}/>
-          <Route path="/schedule" element={<Schedule/>}/>
-
-          <Route path="/my_page" element={<MyPage/>}/>
-          <Route path="/checkPw" element={<CheckPw/>}/>
-          <Route path="/update" element={<Update/>}/>
-          <Route path="/notice" element={<Notice/>}/>
-
-
+          <Route path="/my_team" element={<MyTeam />} />
+          <Route path="/my_recruit_team" element={<MyRecruitTeam />} />
+          <Route path="/my_team_applicant/:id" element={<MyTeamApplicant />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/my_page" element={<MyPage />} />
+          <Route path="/checkPw" element={<CheckPw />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/notice" element={<Notice />} />
         </Routes>
-
 
         <Footer />
       </div>
---
+++ src/components/MyTeam/My_team.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import styles from "../../styles/My_team.module.css"; // CSS 파일 임포트
+import { teams } from "../mockData"; // mock 데이터 임포트
+
+const TeamMember = ({ members }) => {
+  return (
+    <div className={styles.teamMember}>
+      {members.map((member) => (
+        <div key={member.id} className={styles.member_item}>
+          <div className={styles.svg}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="58"
+              height="58"
+              viewBox="0 0 58 58"
+              fill="none"
+            >
+              <rect
+                width="58"
+                height="58"
+                rx="29"
+                fill="#7F7F7F"
+                fillOpacity="0.2"
+                style={{ mixBlendMode: "luminosity" }}
+              />
+              <rect
+                width="58"
+                height="58"
+                rx="29"
+                fill="#3D3D3D"
+                fillOpacity="0.5"
+                style={{ mixBlendMode: "overlay" }}
+              />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M37.7004 23.2C37.7004 28.0049 33.8052 31.9 29.0004 31.9C24.1955 31.9 20.3004 28.0049 20.3004 23.2C20.3004 18.3951 24.1955 14.5 29.0004 14.5C33.8052 14.5 37.7004 18.3951 37.7004 23.2ZM34.8004 23.2C34.8004 26.4033 32.2036 29 29.0004 29C25.7971 29 23.2004 26.4033 23.2004 23.2C23.2004 19.9967 25.7971 17.4 29.0004 17.4C32.2036 17.4 34.8004 19.9967 34.8004 23.2Z"
+                fill="#8B0B02"
+              />
+              <path
+                d="M29.0004 36.25C19.6125 36.25 11.6138 41.8012 8.56689 49.5785C9.30914 50.3155 10.091 51.0127 10.9091 51.6665C13.178 44.5262 20.2956 39.15 29.0004 39.15C37.7051 39.15 44.8227 44.5262 47.0916 51.6665C47.9097 51.0127 48.6916 50.3155 49.4338 49.5785C46.3869 41.8012 38.3882 36.25 29.0004 36.25Z"
+                fill="#8B0B02"
+              />
+            </svg>
+          </div>
+
+          <div className={styles.info}>
+            <div className={styles.name}>
+              <h>{member.name}</h>
+            </div>
+            <div className={styles.department_num}>
+              {member.stuNum}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="6"
+                height="5"
+                viewBox="0 0 6 5"
+                fill="none"
+              >
+                <ellipse
+                  cx="3.16874"
+                  cy="2.49077"
+                  rx="2.52762"
+                  ry="2.49077"
+                  fill="black"
+                />
+              </svg>
+              {member.department}
+            </div>
+            <div className={styles.email}>{member.email}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const MyTeam = () => {
+  const [teamList, setTeamList] = useState(teams); // 모든 팀 데이터를 가져옴
+
+  return (
+    <div>
+      <div className={styles.Myteam_container}>
+        <div className={styles.Myteam_header}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="33"
+            height="33"
+            viewBox="0 0 33 33"
+            fill="none"
+          >
+            <path
+              d="M22 5.5H24.75C25.4793 5.5 26.1788 5.78973 26.6945 6.30546C27.2103 6.82118 27.5 7.52066 27.5 8.25V27.5C27.5 28.2293 27.2103 28.9288 26.6945 29.4445C26.1788 29.9603 25.4793 30.25 24.75 30.25H8.25C7.52065 30.25 6.82118 29.9603 6.30546 29.4445C5.78973 28.9288 5.5 28.2293 5.5 27.5V8.25C5.5 7.52066 5.78973 6.82118 6.30546 6.30546C6.82118 5.78973 7.52065 5.5 8.25 5.5H11M12.375 2.75H20.625C21.3844 2.75 22 3.36561 22 4.125V6.875C22 7.63439 21.3844 8.25 20.625 8.25H12.375C11.6156 8.25 11 7.63439 11 6.875V4.125C11 3.36561 11.6156 2.75 12.375 2.75Z"
+              stroke="#8B0B02"
+              stroke-width="2.91667"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <h>MY 팀플</h>
+        </div>
+        {teamList.map((team, index) => (
+          <div key={index} className={styles.oneteam}>
+            <div className={styles.teamName}>
+              {team.teamName} {/* 팀 이름을 출력 */}
+              <button className={styles.schedule_add}>일정추가</button>
+            </div>
+            <div className={styles.team_member_list}>
+              <p>팀원</p>
+              <TeamMember members={team.members} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyTeam;
