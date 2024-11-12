0f2920c4eb4540a9956e247996acb0c1a839a3b2
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-11-12 19:42:23 +0900

    feat(banner): 로그인 유무에 따른 배너 상태 변경

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,7 @@ const apiClient = axios.create({
   baseURL: "http://15.165.62.195:8080/",
   //   timeout: 5000, 벡엔드와 상의, 벡엔드 타임아웃 보다는 짧게
 
-  headers: { "content-type": "applications/json" },
-
+  headers: { "content-type": "application/json" },
 });
 
 export default apiClient;
