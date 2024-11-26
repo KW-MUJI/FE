7188970ac4bbae668e92ba241c334b41bd82c994
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-11-26 17:56:40 +0900

    fix(team): 포트폴리오 수정일 표시형식 변경

--- src/utils/dateUtil.js
+++ src/utils/dateUtil.js
@@ -4,3 +4,11 @@ export const formatDate = (date) => {
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 };
+
+export const iostringFormatDate = (isoString) => {
+  const date = new Date(isoString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
